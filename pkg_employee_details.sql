4d60b831efc0d68846b4ff16ecb0e4a3bc61c999
Somnath Giri <somnathgiri1@outlook.com>
2019-03-25 05:09:57 +0530

    package modified to add body

--- pkg_employee_details.sql
+++ pkg_employee_details.sql
@@ -3,3 +3,10 @@ Procedure p_add_employee
 Procedure p_delete_employee
 Procedure p_remove_employee
 End pkg_employee_details;
+
+create or replace package body pkg_employee_details
+Procedure p_add_employee
+Procedure p_delete_employee
+Procedure p_remove_employee
+End pkg_employee_details;
+
